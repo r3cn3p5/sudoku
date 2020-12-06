91b3008cddf369363cab9bcb70b793d06358204e
spencer <spencer@31prime.co.uk>
2020-12-06 16:57:41 +0000

    Initial

---
+++ src/main/java/suduko/IntPair.java
@@ -0,0 +1,9 @@
+package suduko;
+
+class IntPair {
+
+    final int x;
+    final int y;
+    IntPair(int x, int y) {this.x=x;this.y=y;}
+
+}
---
+++ src/main/java/suduko/SudukoNumber.java
@@ -0,0 +1,93 @@
+package suduko;
+
+public class SudukoNumber {
+
+
+    private boolean[] numberState = new boolean[9];
+
+
+    public SudukoNumber() {
+
+        for ( int i=0; i<numberState.length; i++)
+            numberState[i] = true;
+
+
+    }
+
+    public boolean remove(int number) {
+
+
+        if (numberState[number-1] == false)
+            return false;
+
+        numberState[number-1] = false;
+
+        return true;
+    }
+
+    public void removeAllExcept(int number) {
+
+        for ( int i=0; i<numberState.length; i++)
+            numberState[i] = false;
+
+        numberState[number-1] = true;
+    }
+
+    public boolean isAvailable(int number) {
+
+        return numberState[number-1];
+    }
+
+    public int[] getNumbersAvailable() {
+
+        int[] av_array = new int[availableCount()];
+        int av_index = 0;
+
+        for ( int i=0; i<numberState.length; i++) {
+
+            if ( numberState[i] == true) {
+                av_array[av_index] = i+1;
+                av_index++;
+
+            }
+        }
+
+        return av_array;
+    }
+
+    public int availableCount() {
+
+        int count = 0;
+
+        for ( int i=0; i<numberState.length; i++) {
+
+            if ( numberState[i] == true)
+                count++;
+
+        }
+
+        return count;
+    }
+
+
+    public int getSingle() {
+
+        if (availableCount() == 1)
+            return getNumbersAvailable()[0];
+
+
+        return 0;
+
+    }
+
+    public String printSingle() {
+
+        if (availableCount() == 1)
+            return String.valueOf(getNumbersAvailable()[0]);
+
+
+        return " ";
+
+    }
+
+}
