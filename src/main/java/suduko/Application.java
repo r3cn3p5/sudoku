62c0ea6cf15fc4829f8ddd5f90520a09330a8c59
spencer <spencer@31prime.co.uk>
2021-03-22 23:23:10 +0000

    Working brute force needs refactoring.

--- src/main/java/suduko/Application.java
+++ src/main/java/suduko/Application.java
@@ -30,18 +30,14 @@ public class Application {
                 }
 
                 for (int c=0; c<9; c++) {
-                    int number = Integer.valueOf(line.substring(c,c+1));
-                    if (number!=0)
-                        puzzle[c][lineNumber] = number;
+                    puzzle[lineNumber][c] = Integer.valueOf(line.substring(c,c+1));
                 }
 
                 lineNumber++;
 
                 if (lineNumber == 9) {
-                    s.process();
-                    if (!s)
-                        s.printGrid();
-                    System.out.println("Is this complete " + s.isComplete());
+                    if (s.solve(puzzle,0,0))
+                           System.out.println("Complete ");
 
                 }
             }
@@ -52,76 +48,76 @@ public class Application {
 
     }
 
-    private static void game1(Sudoku s) {
-        s.setCell(3,1,2);
-        s.setCell(3,2,1);
-        s.setCell(4,1,9);
-        s.setCell(5,0,6);
-        s.setCell(5,1,8);
-
-        s.setCell(6,2,5);
-        s.setCell(8,0,4);
-
-        s.setCell(0,5,2);
-        s.setCell(1,3,6);
-        s.setCell(1,4,9);
-        s.setCell(1,5,1);
-        s.setCell(2,3,3);
-
-        s.setCell(6,3,1);
-        s.setCell(6,4,8);
-        s.setCell(8,4,5);
-        s.setCell(8,5,7);
-
-        s.setCell(0,8,5);
-        s.setCell(2,6,9);
-
-        s.setCell(3,6,5);
-        s.setCell(4,6,8);
-        s.setCell(4,8,2);
-        s.setCell(5,8,7);
-
-        s.setCell(7,8,4);
-        s.setCell(8,7,6);
-    }
-
-    private static void game2(Sudoku s) {
-        s.setCell(0,2,8);
-        s.setCell(1,2,4);
-        s.setCell(2,2,1);
-
-        s.setCell(3,1,9);
-        s.setCell(4,2,7);
-
-        s.setCell(6,1,8);
-        s.setCell(6,2,9);
-        s.setCell(8,1,7);
-
-        s.setCell(0,3,1);
-        s.setCell(1,5,2);
-        s.setCell(2,4,3);
-        s.setCell(2,5,5);
-
-        s.setCell(3,4,6);
-        s.setCell(3,5,3);
-        s.setCell(5,3,4);
-        s.setCell(5,4,7);
-
-        s.setCell(6,3,7);
-        s.setCell(6,4,2);
-        s.setCell(7,3,9);
-        s.setCell(8,5,6);
-
-        s.setCell(0,7,3);
-        s.setCell(2,6,2);
-        s.setCell(2,7,9);
-
-        s.setCell(4,6,5);
-        s.setCell(5,7,1);
-
-        s.setCell(6,6,4);
-        s.setCell(7,6,7);
-        s.setCell(8,6,9);
-
-    }
+//    private static void game1(Sudoku s) {
+//        s.setCell(3,1,2);
+//        s.setCell(3,2,1);
+//        s.setCell(4,1,9);
+//        s.setCell(5,0,6);
+//        s.setCell(5,1,8);
+//
+//        s.setCell(6,2,5);
+//        s.setCell(8,0,4);
+//
+//        s.setCell(0,5,2);
+//        s.setCell(1,3,6);
+//        s.setCell(1,4,9);
+//        s.setCell(1,5,1);
+//        s.setCell(2,3,3);
+//
+//        s.setCell(6,3,1);
+//        s.setCell(6,4,8);
+//        s.setCell(8,4,5);
+//        s.setCell(8,5,7);
+//
+//        s.setCell(0,8,5);
+//        s.setCell(2,6,9);
+//
+//        s.setCell(3,6,5);
+//        s.setCell(4,6,8);
+//        s.setCell(4,8,2);
+//        s.setCell(5,8,7);
+//
+//        s.setCell(7,8,4);
+//        s.setCell(8,7,6);
+//    }
+//
+//    private static void game2(Sudoku s) {
+//        s.setCell(0,2,8);
+//        s.setCell(1,2,4);
+//        s.setCell(2,2,1);
+//
+//        s.setCell(3,1,9);
+//        s.setCell(4,2,7);
+//
+//        s.setCell(6,1,8);
+//        s.setCell(6,2,9);
+//        s.setCell(8,1,7);
+//
+//        s.setCell(0,3,1);
+//        s.setCell(1,5,2);
+//        s.setCell(2,4,3);
+//        s.setCell(2,5,5);
+//
+//        s.setCell(3,4,6);
+//        s.setCell(3,5,3);
+//        s.setCell(5,3,4);
+//        s.setCell(5,4,7);
+//
+//        s.setCell(6,3,7);
+//        s.setCell(6,4,2);
+//        s.setCell(7,3,9);
+//        s.setCell(8,5,6);
+//
+//        s.setCell(0,7,3);
+//        s.setCell(2,6,2);
+//        s.setCell(2,7,9);
+//
+//        s.setCell(4,6,5);
+//        s.setCell(5,7,1);
+//
+//        s.setCell(6,6,4);
+//        s.setCell(7,6,7);
+//        s.setCell(8,6,9);
+//
+//    }
 }
--- src/main/java/suduko/Sudoku.java
+++ src/main/java/suduko/Sudoku.java
@@ -13,7 +13,7 @@ public class Sudoku {
             for (int n = 1; n <= 9; n++) {
                 int counter=0;
                 for (int c=0; c<9; c++) {
-                    if (n==puzzle[c][r])
+                    if (n==puzzle[r][c])
                         counter++;
                 }
                 if (counter > 1)
@@ -25,7 +25,7 @@ public class Sudoku {
             for (int n = 1; n <= 9; n++) {
                 int counter=0;
                 for (int r=0; r<9; r++) {
-                    if (n==puzzle[c][r])
+                    if (n==puzzle[r][c])
                         counter++;
                 }
                 if (counter > 1)
@@ -39,8 +39,8 @@ public class Sudoku {
                 for (int n = 1; n <= 9; n++) {
                     int counter=0;
                     for (int c=0; c<3; c++) {
-                        for (int r=0; r<3; c++) {
-                            if (n==puzzle[(sc*3) + c][(sr*3) + r])
+                        for (int r=0; r<3; r++) {
+                            if (n==puzzle[(sr*3) + r][(sc*3) + c])
                                     counter++;
                         }
                     }
@@ -50,69 +50,63 @@ public class Sudoku {
             }
         }
 
-
-
-       return false;
+        return false;
     }
 
 
-    private boolean solve(int[][] puzzle, int cx, int cy) {
+    public boolean solve(int[][] lastPuzzle, int cx, int cy) {
 
-        // not valid return false
+        int puzzle[][] = new int[9][9];
+        for (int r=0; r<9; r++) {
+            for (int c=0; c<9; c++) {
+                puzzle[r][c] = lastPuzzle[r][c];
+            }
+
+        }
+
+        if (cx>=9) {
+            cx = 0;
+            cy++;
+
+            if (cy >= 9) {
+                printGrid(puzzle);
+                return true;
+            }
+        }
+
+            // not valid return false
         if (isError(puzzle))
             return false;
 
-        if (puzzle[cx][cy] != 0) {
-
-            // move next location
-            cx++;
-            if (cx==9) {
-                cx=0;
-                cy++;
-
-                if (cy==9) {
-                    return true;
-                }
-            }
-
-            if (solve(puzzle, cx, cy))
-                return true;
-
+        if (puzzle[cy][cx] != 0) {
+            return solve(puzzle, cx+1, cy);
         } else {
             for (int n = 1; n <= 9; n++) {
 
                 // set next number
-                puzzle[cx][cy] = n;
+                puzzle[cy][cx] = n;
 
                 // move next location
-                cx++;
-                if (cx==9) {
-                    cx=0;
-                    cy++;
-
-                    if (cy==9) {
-                        return true;
-                    }
+                if (solve(puzzle, cx+1, cy)) {
+                    return true;
                 }
 
-                if (solve(puzzle, cx, cy))
-                    return true;
-
             }
+            return false;
         }
 
-        return true;
+        // Should never get here
     }
 
 
     public void printGrid(int[][] puzzle) {
 
         System.out.println("+-----+-----+-----+");
-        for (int y=0; y<6; y++) {
+        for (int y=0; y<9; y++) {
 
             System.out.print("|");
             for (int x=0; x<9; x++) {
-                System.out.print(puzzle[x][y]);
+                System.out.print(puzzle[y][x]);
 
                 if ((x+1) % 3 == 0)
                     System.out.print("|");
